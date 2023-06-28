a2f69746d6ee9f6f05547b0e036a0f0a457f3a96
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-06-28 04:48:22 +0000

    [add]font awesome, boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'homes/top'
+  end
   #　顧客用
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
